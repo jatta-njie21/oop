aabac9a9ff180ec44a6c6a08159001928575e813
jatta-njie21 <laboyjatta@gmail.com>
2020-05-19 15:34:38 +0000

    AAdding packages

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from tester import modulos
 class Parent():
     
     def __init__(self,num1,num2):
@@ -19,10 +20,17 @@ class Parent():
 class Child(Parent):
     pass
 
-test = Parent(45,5)
-test1 = Child(23,3)
 
-test1.add()
+#To run any method, first declare an instance of a class
+#Then use that instance to call a method
+
+
+test = Parent(45,5) #creating an instance for the parent class
+#test1 = Child(23,3) #creating an instance for the child class
+
+#test1.add()
 #test.add()
 #test.sub()
 #test.mul()
+
+test.modulos()
--- tester.py
+++ tester.py
@@ -1,1 +1,6 @@
 
+def modulos(x,y):
+    if x % y == 0:
+        print("0")
+    else:
+        print(x % y)
